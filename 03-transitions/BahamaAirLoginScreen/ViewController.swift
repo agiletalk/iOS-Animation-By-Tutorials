214b655309109dab06cd1e639ba349c843416ca5
chanju Jeon <chanju.jeon@gmail.com>
2018-01-21 03:16:18 +0900

    Challenge 2 문제 해결

--- 03-transitions/BahamaAirLoginScreen/ViewController.swift
+++ 03-transitions/BahamaAirLoginScreen/ViewController.swift
@@ -163,6 +163,7 @@ class ViewController: UIViewController {
                     self.removeMessage(index: index)
                 } else {
                     // reset form
+                    self.resetForm()
                 }
             }
         })
@@ -177,6 +178,21 @@ class ViewController: UIViewController {
             
             self.showMessage(index: index+1)
         })
+    }
+    
+    func resetForm() {
+        UIView.transition(with: status, duration: 0.2, options: .transitionFlipFromTop, animations: {
+            self.status.isHidden = true
+            self.status.center = self.statusPosition
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: [], animations: {
+            self.spinner.center = CGPoint(x: -20.0, y: 16.0)
+            self.spinner.alpha = 0.0
+            self.loginButton.backgroundColor = UIColor(red: 0.63, green: 0.84, blue: 0.35, alpha: 1.0)
+            self.loginButton.bounds.size.width -= 80.0
+            self.loginButton.center.y -= 60.0
+        }, completion: nil)
     }
     
     // MARK: UITextFieldDelegate
