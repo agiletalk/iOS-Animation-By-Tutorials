68d9eb4f6f96e591b78f826950befde3335ef22f
chanju Jeon <chanju.jeon@gmail.com>
2018-03-26 02:15:59 +0900

    starter 프로젝트 완성

--- 19-interactive-uinavigationcontroller-transitions/LogoReveal/RevealAnimator.swift
+++ 19-interactive-uinavigationcontroller-transitions/LogoReveal/RevealAnimator.swift
@@ -22,78 +22,130 @@
 
 import UIKit
 
-class RevealAnimator: NSObject, UIViewControllerAnimatedTransitioning, CAAnimationDelegate {
-
-  let animationDuration = 2.0
-  var operation: UINavigationControllerOperation = .push
-
-  weak var storedContext: UIViewControllerContextTransitioning?
-
-  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-    return animationDuration
-  }
-
-  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-    storedContext = transitionContext
-
-    if operation == .push {
-      let fromVC = transitionContext.viewController(forKey: .from) as! MasterViewController
-      let toVC = transitionContext.viewController(forKey: .to) as! DetailViewController
-
-      transitionContext.containerView.addSubview(toVC.view)
-      toVC.view.frame = transitionContext.finalFrame(for: toVC)
-
-      let animation = CABasicAnimation(keyPath: "transform")
-      animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
-      animation.toValue = NSValue(caTransform3D: CATransform3DConcat(
-        CATransform3DMakeTranslation(0.0, -10.0, 0.0),
-        CATransform3DMakeScale(150.0, 150.0, 1.0)
-      ))
-
-      animation.duration = animationDuration
-      animation.delegate = self
-      animation.fillMode = kCAFillModeForwards
-      animation.isRemovedOnCompletion = false
-      animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
-
-      let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
-      maskLayer.position = fromVC.logo.position
-      toVC.view.layer.mask = maskLayer
-      maskLayer.add(animation, forKey: nil)
-
-      fromVC.logo.add(animation, forKey: nil)
-
-      let fadeIn = CABasicAnimation(keyPath: "opacity")
-      fadeIn.fromValue = 0.0
-      fadeIn.toValue = 1.0
-      fadeIn.duration = animationDuration
-      toVC.view.layer.add(fadeIn, forKey: nil)
-
-    } else {
-
-      let fromView = transitionContext.view(forKey: .from)!
-      let toView = transitionContext.view(forKey: .to)!
-
-      transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
-
-      UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseIn, animations: {
-        fromView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-      }, completion: { _ in
-        transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-      })
+class RevealAnimator: UIPercentDrivenInteractiveTransition, UIViewControllerAnimatedTransitioning, CAAnimationDelegate {
+    
+    let animationDuration = 2.0
+    var operation: UINavigationControllerOperation = .push
+    
+    weak var storedContext: UIViewControllerContextTransitioning?
+    
+    var interactive = false
+    private var pausedTime: CFTimeInterval = 0
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
     }
-  }
-
-  func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-    if let context = storedContext {
-      context.completeTransition(!context.transitionWasCancelled)
-      //reset logo
-      let fromVC = context.viewController(forKey: .from) as! MasterViewController
-      fromVC.logo.removeAllAnimations()
-
-      let toVC = context.viewController(forKey: .to) as! DetailViewController
-      toVC.view.layer.mask = nil
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if interactive {
+            let transitionLayer = transitionContext.containerView.layer
+            pausedTime = transitionLayer.convertTime(CACurrentMediaTime(), from: nil)
+            transitionLayer.speed = 0
+            transitionLayer.timeOffset = pausedTime
+        }
+        
+        storedContext = transitionContext
+        
+        if operation == .push {
+            let fromVC = transitionContext.viewController(forKey: .from) as! MasterViewController
+            let toVC = transitionContext.viewController(forKey: .to) as! DetailViewController
+            
+            transitionContext.containerView.addSubview(toVC.view)
+            toVC.view.frame = transitionContext.finalFrame(for: toVC)
+            
+            let animation = CABasicAnimation(keyPath: "transform")
+            animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+            animation.toValue = NSValue(caTransform3D: CATransform3DConcat(
+                CATransform3DMakeTranslation(0.0, -10.0, 0.0),
+                CATransform3DMakeScale(150.0, 150.0, 1.0)
+            ))
+            
+            animation.duration = animationDuration
+            animation.delegate = self
+            animation.fillMode = kCAFillModeForwards
+            animation.isRemovedOnCompletion = false
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+            
+            let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
+            maskLayer.position = fromVC.logo.position
+            toVC.view.layer.mask = maskLayer
+            maskLayer.add(animation, forKey: nil)
+            
+            fromVC.logo.add(animation, forKey: nil)
+            
+            let fadeIn = CABasicAnimation(keyPath: "opacity")
+            fadeIn.fromValue = 0.0
+            fadeIn.toValue = 1.0
+            fadeIn.duration = animationDuration
+            toVC.view.layer.add(fadeIn, forKey: nil)
+            
+        } else {
+            
+            let fromView = transitionContext.view(forKey: .from)!
+            let toView = transitionContext.view(forKey: .to)!
+            
+            transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
+            
+            UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseIn, animations: {
+                fromView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+            }, completion: { _ in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            })
+        }
     }
-    storedContext = nil
-  }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if let context = storedContext {
+            context.completeTransition(!context.transitionWasCancelled)
+            //reset logo
+            let fromVC = context.viewController(forKey: .from) as! MasterViewController
+            fromVC.logo.removeAllAnimations()
+            
+            let toVC = context.viewController(forKey: .to) as! DetailViewController
+            toVC.view.layer.mask = nil
+        }
+        storedContext = nil
+    }
+    
+    func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        let transition = recognizer.translation(in: recognizer.view!.superview!)
+        var progress: CGFloat = abs(transition.x / 200.0)
+        progress = min(max(progress, 0.01), 0.99)
+        
+        switch recognizer.state {
+        case .changed:
+            update(progress)
+        case .cancelled, .ended:
+            if progress < 0.5 {
+                cancel()
+            } else {
+                finish()
+            }
+            interactive = false
+        default:
+            break
+        }
+    }
+    
+    override func update(_ percentComplete: CGFloat) {
+        super.update(percentComplete)
+        let animationProgress = TimeInterval(animationDuration) * TimeInterval(percentComplete)
+        storedContext?.containerView.layer.timeOffset = pausedTime + animationProgress
+    }
+    
+    override func cancel() {
+        restart(forFinishing: false)
+        super.cancel()
+    }
+    
+    override func finish() {
+        restart(forFinishing: true)
+        super.finish()
+    }
+    
+    private func restart(forFinishing: Bool) {
+        let transitionLayer = storedContext?.containerView.layer
+        transitionLayer?.beginTime = CACurrentMediaTime()
+        transitionLayer?.speed = forFinishing ? 1 : -1
+    }
 }
