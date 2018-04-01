b3ec4378ae780872e50b2856bf7fc5960452f302
chanju Jeon <chanju.jeon@gmail.com>
2018-04-02 03:17:02 +0900

    starter 프로젝트 완성

--- 21-animator-keyframe-animations/LockSearch/WidgetCell.swift
+++ 21-animator-keyframe-animations/LockSearch/WidgetCell.swift
@@ -44,9 +44,28 @@ class WidgetCell: UITableViewCell {
 
     self.showsMore = !self.showsMore
 
-    self.widgetHeight.constant = self.showsMore ? 230 : 130
-    self.tableView?.reloadData()
-
+    let animations = {
+      self.widgetHeight.constant = self.showsMore ? 230 : 130
+      if let tableView = self.tableView {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        tableView.layoutIfNeeded()
+      }
+    }
+    
+    let textTransition = {
+      UIView.transition(with: sender, duration: 0.25, options: .transitionCrossDissolve, animations: {
+        sender.setTitle(self.showsMore ? "Show Less" : "Show More", for: .normal)
+      }, completion: nil)
+    }
+    
+    let spring = UISpringTimingParameters(mass: 30, stiffness: 1000, damping: 300, initialVelocity: CGVector(dx: 5, dy: 0))
+    
+    toggleHeightAnimator = UIViewPropertyAnimator(duration: 0.0, timingParameters: spring)
+    toggleHeightAnimator?.addAnimations(animations)
+    toggleHeightAnimator?.addAnimations(textTransition, delayFactor: 0.5)
+    toggleHeightAnimator?.startAnimation()
+    
     widgetView.expanded = showsMore
     widgetView.reload()
   }
