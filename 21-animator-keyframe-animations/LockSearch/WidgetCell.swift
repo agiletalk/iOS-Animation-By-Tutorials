ab675a8989504dd7cada942fd83846220867b428
chanju Jeon <chanju.jeon@gmail.com>
2018-04-03 01:59:43 +0900

    Challenge 1 문제 해결

--- 21-animator-keyframe-animations/LockSearch/WidgetCell.swift
+++ 21-animator-keyframe-animations/LockSearch/WidgetCell.swift
@@ -59,12 +59,19 @@ class WidgetCell: UITableViewCell {
       }, completion: nil)
     }
     
-    let spring = UISpringTimingParameters(mass: 30, stiffness: 1000, damping: 300, initialVelocity: CGVector(dx: 5, dy: 0))
-    
-    toggleHeightAnimator = UIViewPropertyAnimator(duration: 0.0, timingParameters: spring)
-    toggleHeightAnimator?.addAnimations(animations)
-    toggleHeightAnimator?.addAnimations(textTransition, delayFactor: 0.5)
-    toggleHeightAnimator?.startAnimation()
+    if let toggleHeightAnimator = toggleHeightAnimator, toggleHeightAnimator.isRunning {
+      toggleHeightAnimator.pauseAnimation()
+      toggleHeightAnimator.addAnimations(animations)
+      toggleHeightAnimator.addAnimations(textTransition, delayFactor: 0.5)
+      toggleHeightAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 1.0)
+    } else {
+      let spring = UISpringTimingParameters(mass: 30, stiffness: 1000, damping: 300, initialVelocity: CGVector(dx: 5, dy: 0))
+      
+      toggleHeightAnimator = UIViewPropertyAnimator(duration: 0.0, timingParameters: spring)
+      toggleHeightAnimator?.addAnimations(animations)
+      toggleHeightAnimator?.addAnimations(textTransition, delayFactor: 0.5)
+      toggleHeightAnimator?.startAnimation()
+    }
     
     widgetView.expanded = showsMore
     widgetView.reload()
